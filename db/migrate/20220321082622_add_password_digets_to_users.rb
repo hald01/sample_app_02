b9a0c705dc6f569ee3dccede2eedb897105c382f
hald19 <dangha190297@gmail.com>
2022-03-21 17:43:39 +0700

    add sign up 10%

--- db/migrate/20220321082622_add_password_digets_to_users.rb
+++ db/migrate/20220321082622_add_password_digets_to_users.rb
@@ -1,5 +1,5 @@
 class AddPasswordDigetsToUsers < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :password_digets, :string
+    add_column :users, :password_digest, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_21_082622) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "password_digets"
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
