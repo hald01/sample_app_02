d4ee22992540804477f5549279d8eb69c4cd470e
hald19 <dangha190297@gmail.com>
2022-03-29 17:42:33 +0700

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -10,14 +10,21 @@ Rails.application.routes.draw do
   get '/contact', to: 'static_pages#contact'
 
   get '/signup',  to: 'users#new'
-  resources :users # ~ get 'users/:id', to: 'users#show'
+  #resources :users # ~ get 'users/:id', to: 'users#show'
   #get '/delete', to: 'users#destroy'
 
   get '/login', to: 'sessions#new'
   post '/login',  to: 'sessions#create'  
   delete '/logout',  to: 'sessions#destroy'  
 
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -38,3 +38,10 @@ for a in 5..100 do
                 activated_at: Time.zone.now
             )    
 end
+# Create following relationships.
+users = User.all
+user = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
